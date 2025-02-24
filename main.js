914b68882013cb7905a332f42a7bd424f6215e96
Money <manhtien310701@gmail.com>
2025-02-24 17:54:10 +0800

    update crawl-data-group-page

--- main.js
+++ main.js
@@ -44,67 +44,15 @@ async function main() {
       await window1.webContents.executeJavaScript(scrapeDataFromMessagePage(data.account))
 
       // Close the window
-      window1.close();
+      // window1.close();
     }
 
-    const runTask1 = data.map(async (item) => {
-      await task1(item)
-      return
-    })
+    // const runTask1 = data.map(async (item) => {
+    //   await task1(item)
+    //   return
+    // })
 
-    // const taskMain = async () => {
-    //   // Load the url of the facebook (Login FB)
-    //   await mainWindow.loadURL('https://www.facebook.com/')
-
-    //   // Loop fetch group data by page
-    //   const hasGroupData = true
-    //   let page = 0
-    //   while (hasGroupData) {
-    //     const urlGroupData = await fetchGroupData(page) // Call the function to fetch group data
-    //     console.log('Page: ', page + 1)
-    //     if (!urlGroupData) break
-
-    //     for (const url of urlGroupData) {
-    //       let urlAccess = url
-    //       // Load the url of the group facebook
-    //       if (urlAccess.includes('share')) {
-    //         await mainWindow.loadURL(urlAccess)
-    //         urlAccess = mainWindow.webContents.getURL().split('?')[0].split('#')[0]
-    //       }
-    //       console.log('url: ', urlAccess)
-    //       if (!urlAccess.includes('https://www.facebook.com')) continue;
-    //       await mainWindow.loadURL(`${urlAccess.replace(/\/$/, "")}/search?q=zalo`)
-    //       await delay(5000)
-
-
-    //       // Scrape data from browser
-    //       const data = await mainWindow.webContents.executeJavaScript(scrapeDataFromBrowser)
-    //       console.log('data length: ', data.length)
-    //       if (!!data?.length) {
-    //         const saveData = await saveDataToDatabase(JSON.stringify(data))
-    //         await mainWindow.webContents.executeJavaScript(`
-    //           (async () => {
-    //             console.log('saveData: ', ${JSON.stringify(saveData)})
-    //           })()
-    //         `)
-    //         const transformData = await transformDataByChatgpt()
-    //         await mainWindow.webContents.executeJavaScript(`
-    //           (async () => {
-    //             console.log('transformData: ', ${JSON.stringify(transformData)})
-    //           })()
-    //         `)
-    //       }
-
-    //       await delay(1000) // Wait for 10 seconds
-    //     }
-    //     page++
-    //   }
-
-    //   // Load the index.html in project of the desktop app.
-    //   await mainWindow.loadFile('index.html')
-    // }
-
-    const task2 = async () => {
+    const taskMain = async () => {
       // Load the url of the facebook (Login FB)
       await mainWindow.loadURL('https://www.facebook.com/')
 
@@ -125,8 +73,9 @@ async function main() {
           }
           console.log('url: ', urlAccess)
           if (!urlAccess.includes('https://www.facebook.com')) continue;
-          await mainWindow.loadURL(urlAccess.replace(/\/$/, ""))
+          await mainWindow.loadURL(`${urlAccess.replace(/\/$/, "")}/search?q=zalo`)
           await delay(5000)
+
 
           // Scrape data from browser
           const data = await mainWindow.webContents.executeJavaScript(scrapeDataFromBrowser)
@@ -155,7 +104,44 @@ async function main() {
       await mainWindow.loadFile('index.html')
     }
 
-    await Promise.all([runTask1, task2()])
+    const task2 = async () => {
+      // Load the url of the facebook (Login FB)
+      await mainWindow.loadURL('https://www.facebook.com/')
+
+      // Loop fetch group data by page
+      const hasGroupData = true
+      let page = 0
+      while (hasGroupData) {
+        const urlGroupData = await fetchGroupData(page) // Call the function to fetch group data
+        console.log('Page: ', page + 1)
+        if (!urlGroupData) break
+
+        for (const url of urlGroupData) {
+          console.log('url: ', url)
+          if (!url.includes('https://www.facebook.com')) continue;
+          await mainWindow.loadURL(url)
+          await delay(5000)
+
+          // Scrape data from browser
+          const data = await mainWindow.webContents.executeJavaScript(scrapeDataFromGroupPage())
+          console.log('data length: ', data.length)
+          if (!!data?.length) {
+            const saveData = await saveDataToDatabase(JSON.stringify(data))
+            console.log('saveData: ', saveData)
+            const transformData = await transformDataByChatgpt()
+            console.log('transformData: ', transformData)
+          }
+
+          await delay(1000) // Wait for 10 seconds
+        }
+        page++
+      }
+
+      // Load the index.html in project of the desktop app.
+      await mainWindow.loadFile('index.html')
+    }
+
+    await Promise.all([task2()])
   });
 
   // Open the DevTools. (Ctr + Shift + I)
@@ -306,21 +292,21 @@ async function transformDataByChatgpt() {
   }
 }
 
-// Function to scrape the data from the browser (group page)
+// Function to scrape the data from the browser (group page wiyh keyword='zalo')
 const scrapeDataFromBrowser = `(async () => {
   const delay = async (time) => {
     await new Promise(resolve => setTimeout(resolve, time));
   }
   try {
     await delay(1000)
-    const documentPage = document?.querySelector('div[role="feed"]')
+    const documentPage = document?.querySelector('.x193iq5w.x1xwk8fm')
     console.log('documentPage: ', documentPage)
     if (!documentPage) return [] // If the documentPage is not found, return an empty array
 
     // Get text of group name
     await delay(1000)
-    const groupName = document?.querySelector('div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x193iq5w.xs83m0k.xsyo7zv.x16hj40l.x1yrsyyn.x1r8uery.x1dh0t33.x17upfok.x1l90r2v > div > div > div > div:nth-child(1) > div > div:nth-child(1) > h2 > span > a')?.textContent
-    // const groupName = elementGroupName?.split(' ')?.slice(1)?.join(' ')
+    const elementGroupName = document?.querySelector('div.x9f619.x1ja2u2z.x78zum5.x2lah0s.x1n2onr6.x1qughib.x6s0dn4.xozqiw3.x1q0g3np.x1sy10c2.xktsk01.xod5an3.x1d52u69 > div > div > div > div > div:nth-child(2) > span > span')?.textContent
+    const groupName = elementGroupName?.split(' ')?.slice(1)?.join(' ')
 
     await delay(1000)
     let elementArr = documentPage?.querySelectorAll('.x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z')
@@ -384,6 +370,91 @@ const scrapeDataFromBrowser = `(async () => {
     return []
   }
 })()`
+
+// Function to scrape the data from the browser (group page)
+const scrapeDataFromGroupPage = () => {
+  return `(async () => {
+    const delay = async (time) => {
+      await new Promise(resolve => setTimeout(resolve, time));
+    }
+    try {
+      await delay(1000)
+      const documentPage = document?.querySelector('div[role="feed"]')
+      console.log('documentPage: ', documentPage)
+      if (!documentPage) return [] // If the documentPage is not found, return an empty array
+
+      // Get text of group name
+      await delay(1000)
+      const groupName = document?.querySelector('.x1e56ztr.x1xmf6yo > [dir="auto"] > span[dir="auto"] > a[role="link"]')?.textContent
+      // const groupName = elementGroupName?.split(' ')?.slice(1)?.join(' ')
+
+      await delay(1000)
+      let elementArr = documentPage?.querySelectorAll('.x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z')
+      // console.log('elementArr: ', elementArr.length)
+      if (!elementArr || !elementArr.length) return [] // If the elementArr is not found or empty, return an empty array
+
+      let data = []
+      for (let i = 0; i < elementArr.length; i++) {
+        await delay(1000)
+        // Scroll to the element ith
+        elementArr[i]?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+        await delay(1000)
+        const btnSeeMore = elementArr[i]?.querySelector('.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1sur9pj.xkrqix3.xzsf02u.x1s688f[role="button"]')
+        if (btnSeeMore) {
+          btnSeeMore.scrollIntoView({ behavior: 'smooth', block: 'center' });
+          btnSeeMore.click()
+          await delay(1000)
+        }
+
+        // Scrape text content of the element
+        await delay(1000)
+        let textContent = elementArr[i]?.querySelector('.x1yx25j4.x13crsa5.x1rxj1xn.xxpdul3.x6x52a7')?.textContent
+        if (!textContent) textContent = elementArr[i]?.querySelector('.html-div.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x1l90r2v.x1pi30zi.x1swvt13.x1iorvi4')?.textContent
+        if (!textContent) textContent = elementArr[i]?.querySelectorAll('span[dir="auto"].x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x3x7a5m.x6prxxf.xvq8zen.xo1l8bm.xzsf02u.x1yc453h')[2]?.textContent
+        if (!textContent) textContent = elementArr[i]?.querySelectorAll('span[dir="auto"].x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x3x7a5m.x6prxxf.xvq8zen.xo1l8bm.xzsf02u.x1yc453h')[1]?.textContent
+        if (!textContent) textContent = elementArr[i]?.querySelectorAll('span[dir="auto"].x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x3x7a5m.x6prxxf.xvq8zen.xo1l8bm.xzsf02u.x1yc453h')[0]?.textContent
+        if (!textContent) textContent = elementArr[i]?.querySelector('.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x1vvkbs.x126k92a')?.textContent
+        if (!textContent) textContent = elementArr[i]?.querySelector('.html-div.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x1swvt13.x1pi30zi.x18d9i69')?.textContent || ''
+        const textAccount = elementArr[i]?.querySelector('.html-h3')?.textContent || ''
+        const textIdAccount = elementArr[i]?.querySelector('.html-h3 a')?.href?.split('/')[6] || ''
+
+        if (textContent) {
+          data.push({ content: textContent, group: groupName, account: textAccount, idAccount: textIdAccount, crawlBy: 'shanghaifanyuan613@gmail.com', userId: 2, type: 'comment' })
+        }
+
+        if (i < 25 || data.length < 25) {
+          await delay(2000)
+          elementArr = documentPage?.querySelectorAll('.x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z')
+        } else {
+          break
+        }
+
+        console.log('data: ', data.length)
+      }
+
+      // Remove duplicate comment and add field contactUs
+      data = data.filter((item, index, self) =>
+        index === self.findIndex((c) => c.content === item.content)
+      ).map((c) => {
+        const contactUs = Array.from(new Set(c?.content?.match(/\\+?\\d{1,3}(?:[.\\s]?\\d{1,4})+|\\b0\\d{9}\\b/g)
+          ?.filter((num, index, self) =>
+            self.indexOf(num) === index && num.replace(/\\D/g, '').length >= 9
+          ) || []))
+          ?.join(', ') || '';
+        return { ...c, contactUs };
+      })
+
+      // Remove data if contactUs is empty
+      data = data.filter((c) => c.contactUs)
+
+      return data
+    } catch (error) {
+      console.log('Error scraping data from browser: ', error)
+      return []
+    }
+  })()`
+}
 
 // Function to scrape the data from the browser (message page)
 const scrapeDataFromMessagePage = (accountCrawl) => {
