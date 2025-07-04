bc716c749fba2072bfb605ab944613a611f7980e
Money <manhtien310701@gmail.com>
2025-07-04 12:02:27 +0800

    add cron-task

---
+++ cron.js
@@ -0,0 +1,28 @@
+const cron = require('node-cron');
+
+// Time task fb
+const timeTaskScrapeFb = async (task) => {
+  try {
+    // Run task at 6:30 AM every day (minute/hour/dayOfMonth/month/dayOfWeek)
+    const taskAt = cron.schedule('01 12 * * *', async () => {
+      try {
+        await task(); // Call the async task
+      } catch (err) {
+        console.error('Error during scheduled task:', err);
+      }
+    });
+    taskAt.start();
+    // Stop the task at 6:00 PM every day
+    const stopTaskAt6PM = cron.schedule('21 11 * * *', () => {
+      taskAt.stop();
+    });
+    stopTaskAt6PM.start();
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+};
+
+module.exports = {
+  timeTaskScrapeFb
+}
