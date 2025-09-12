58fe24e67a228e82118a25c82a90b91dc9b4e1b4
Money <manhtien310701@gmail.com>
2025-09-12 16:28:24 +0800

    fix update-field

--- main.js
+++ main.js
@@ -453,7 +453,7 @@ const scrapeDataFromBrowser = `(async () => {
       if (!textContent) textContent = elementArr[i]?.querySelector('div.x9f619.x2lah0s.x1n2onr6.x78zum5.x1iyjqo2.x1t2pt76.x1lspesw > div > div > div > div > div > div:nth-child(5) > div > div > div > div > div > div > div > div > div > div > div.html-div.xdj266r.x14z9mp.xat24cr.x1lziwak.xexx8yu.xyri2b.x18d9i69.x1c1uobl > div > div > div:nth-child(3) > div > div > div > div')?.textContent
       console.log('Text Content-------: ', textContent)
       const textAccount = elementArr[i]?.querySelector('.html-h3')?.textContent || ''
-      const textIdAccount = elementArr[i]?.querySelector('.html-h3 a')?.href?.split('/')?.[6] || '0'
+      const textIdAccount = elementArr[i]?.querySelector('.html-h3 a')?.href?.split('/')?.[6] || ''
       const urlAvatar = elementArr[i]?.querySelector('g > image')?.href?.baseVal || ''
       await delay(1000)
       const elementUrlContent = elementArr[i]?.querySelectorAll('span:nth-child(1) > span > span > a[role="link"]')[2] ||
